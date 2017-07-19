03009ff54a3e7d3e3c392423350eb0616c671807
i849308 <xin.wen@sap.com>
2017-07-19 15:48:21 -0700

    Chapter 1-3: Plan a redux app - Prepare for ACTIONS & sample data

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+const constants = {
+    ADD_DAY: 'ADD_DAY',
+    REMOVE_DAY: 'REMOVE_DAT',
+    SET_GOAL: "SET_GOAL",
+    ADD_ERROR: "ADD_ERROR",
+    CLEAR_ERROR: "CLEAR_ERROR",
+    FETCH_RESORT_NAMES: "FETCH_RESORT_NAMES",
+    CANCEL_FETCHING: "CANCEL_FETCHING",
+    CHANGE_SUGGESTIONS: "CHANGE_SUGGESTIONS",
+    CLEAR_SUGGESTIONS: "CLEAR_SUGGESTIONS"
+}
+// constants.ADD_DAY - store all action in hash
+
+export default constants
